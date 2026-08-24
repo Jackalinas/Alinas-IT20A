ae96f7fa1ca8b3283547b78c07117c47cd3c5bf3
jack kressley A. Alinas <jackalinas098@gmail.com>
2026-08-24 11:58:09 +0800

    Added Array activity

--- alinasIT20A/src/multiArray.java
+++ alinasIT20A/src/multiArray.java
@@ -7,10 +7,21 @@
  *
  * @author admin
  */
-public class preliem {
+public class multiArray {
     public static void main (String args[] ) {
-System.out.print("IT ROCKS!");
-        System.out.println("HI");
+    int marks[][]={
+        
+        {10,20,30},
+        {40,50,60},
+        {70,80,90}
+    };
+    for (int i=0;i<marks.length;i++){
+    for (int j=0;j<marks[i].length;j++){
+    System.out.print(marks[i][j]+ " ");
+}
+System.out.println();
     }
-    
+        
+        
+}
 }
---
+++ alinasIT20A/src/singleArray.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author admin
+ */
+public class singleArray {
+    public static void main (String args[] ) {
+int number[]={1, 2, 3, 4, 5};
+        for (int i=0;i<number.length;i++){
+        System.out.print(number[i]+" ");
+            
+            
+        }
+    }
+        
+}
